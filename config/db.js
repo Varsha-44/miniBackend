67cf5cae40f325c1ecf940eaf9658814859fc84c
Varsha-44 <121934954+Varsha-44@users.noreply.github.com>
2024-12-27 23:28:09 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,13 @@
-// backend/config/db.js
-
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); // No deprecated options
     console.log('MongoDB connected...');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
-    process.exit(1);
+    process.exit(1); // Exit the process with a failure code
   }
 };
 
